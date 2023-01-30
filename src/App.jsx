e2d77d198ee8796305cd24f43811a00271394c34
Joakim Villo <villokodehode@gmail.com>
2023-01-30 10:40:45 +0100

    finelized convertion

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,44 @@ import VICTOR from "./assets/image-victor.jpg";
 function App() {
   return (
     <>
-      <div class="BackgroundCircles absolute h-screen w-screen bg-design-DarkCyan" />
-      <div class=" h-screen flex justify-center items-center relative overflow-hidden font-Kumbhsans">
-        <div class="grid grid-rows-[1fr_2fr] rounded-xl shadow-xl w-80 h-96 relative bg-white z-50">
-          <div class={`rounded-t-xl bg-cover`}>
-            <img class="rounded-t-xl w-full" src={BGCARD} alt="" />
+      <div className="BackgroundCircles absolute h-screen w-screen bg-design-DarkCyan" />
+      <div className=" h-screen flex justify-center items-center relative overflow-hidden font-Kumbhsans">
+        <div className="grid grid-rows-[1fr_2fr] rounded-xl shadow-xl w-80 h-96 relative bg-white z-50">
+          <div className={`rounded-t-xl bg-cover`}>
+            <img className="rounded-t-xl w-full" src={BGCARD} alt="" />
           </div>
-          <div class="absolute h-28 w-28 right-1/2 top-1/3 -translate-y-1/2 translate-x-1/2">
+          <div className="absolute h-28 w-28 right-1/2 top-1/3 -translate-y-1/2 translate-x-1/2">
             <img
               src={VICTOR}
               alt="picture of Victor Crest"
-              class="rounded-full border-4 border-white"
+              className="rounded-full border-4 border-white"
             />
           </div>
-          <div class="flex flex-col pt-8 justify-center items-center">
-            <h1 class="font-bold text-lg">
+          <div className="flex flex-col pt-8 justify-center items-center">
+            <h1 className="font-bold text-lg">
               Victor Crest{" "}
-              <span class="text-design-DarkGray font-medium pl-2">26</span>
+              <span className="text-design-DarkGray font-medium pl-2">26</span>
             </h1>
-            <p class="text-design-DarkGray text-sm">London</p>
+            <p className="text-design-DarkGray text-sm">London</p>
           </div>
-          <div class="flex justify-around border-t p-6">
-            <div class="text-center text-lg">
-              <p class="font-bold">80K</p>
-              <p class="text-design-DarkGray text-xs tracking-wider">
+          <div className="flex justify-around border-t p-6">
+            <div className="text-center text-lg">
+              <p className="font-bold">80K</p>
+              <p className="text-design-DarkGray text-xs tracking-wider">
                 Followers
               </p>
             </div>
-            <div class="text-center">
-              <p class="font-bold text-lg">803K</p>
-              <p class="text-design-DarkGray text-xs tracking-wider">Likes</p>
+            <div className="text-center">
+              <p className="font-bold text-lg">803K</p>
+              <p className="text-design-DarkGray text-xs tracking-wider">
+                Likes
+              </p>
             </div>
-            <div class="text-center">
-              <p class="font-bold text-lg">1.4K</p>
-              <p class="text-design-DarkGray text-xs tracking-wider">Photos</p>
+            <div className="text-center">
+              <p className="font-bold text-lg">1.4K</p>
+              <p className="text-design-DarkGray text-xs tracking-wider">
+                Photos
+              </p>
             </div>
           </div>
         </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
     extend: {
       fontFamily: { Kumbhsans: ["Kumbh Sans", "sans-serif"] },
